64df8bac13f4a346f4ae28dcc0951be9c8ce4abc
Yigit Erzen <144549821+YigitErzen@users.noreply.github.com>
2025-10-18 22:41:10 +0300

    Add files via upload

---
+++ Lesson 31/Lesson 31/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+string userInput = Console.ReadLine();
+
+if (userInput == "A")
+{
+    writeSelectedOption("Alex");
+}
+else if (userInput == "B")
+{
+    writeSelectedOption("Bella");
+}
+else if (userInput == "L")
+{
+    writeSelectedOption("Lily");
+}
+else
+{
+    writeSelectedOption("Nobody selected!");
+}
+    void writeSelectedOption(string selectedOption)
+    {
+        Console.WriteLine("Selected Person: " + selectedOption);
+    }
