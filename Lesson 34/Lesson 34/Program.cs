6585c5b8e1a641752e82b807cc75af5b473a6eff
Yigit Erzen <yigterzen@gmail.com>
2025-10-24 15:06:24 +0300

    Move lesson source files and remove old projects
    Relocated Program.cs files from individual lesson folders to a unified Lessons directory and deleted all related solution, project, build, and output files for Lessons 14, 19, 21, 27, 31, and 34. This simplifies project structure and centralizes lesson code.

--- Lesson 34/Lesson 34/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿Console.WriteLine("Enter a number");
-string userInput = Console.ReadLine();
-int number = int.Parse(userInput);
-Console.WriteLine(number);
---
+++ Lessons/Lesson 21.cs
@@ -0,0 +1,11 @@
+﻿int a = 10;
+int b = 5;
+
+a++;
+b--;
+
+Console.WriteLine("Addition:" + (a + b));
+Console.WriteLine("Substraction: " + (a - b));
+Console.WriteLine("Substraction: " + a*b);
+Console.WriteLine("Substraction: " + a/b);
+Console.WriteLine("John" + " " + "Smith");
