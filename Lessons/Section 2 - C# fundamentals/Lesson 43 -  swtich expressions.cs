e106590acd3b5a614d45879ce2ad2507177fe493
Yigit Erzen <yigterzen@gmail.com>
2025-12-02 15:57:43 +0300

    Add multiple lessons with console input/output examples and switch expressions

---
+++ Lessons/Section 2 - C# fundamentals/Lesson 43 -  swtich expressions.cs
@@ -0,0 +1,17 @@
+﻿string convertPointsToGrade(int points)
+{
+    return points switch
+    {
+        >= 90 => "AA",
+        >= 80 => "BB",
+        >= 50 => "CC",
+        _ => "DD",
+    };
+}
+
+
+string grade = convertPointsToGrade(90);
+
+Console.WriteLine(grade);
+
+
